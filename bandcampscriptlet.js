840f45b35494eee0ffe7ed9f384f682f37e71914
cremasterjedi <86903788+cremasterjedi@users.noreply.github.com>
2025-08-24 00:26:53 -0400

    Update bandcampscriptlet.js

--- bandcampscriptlet.js
+++ bandcampscriptlet.js
@@ -1,14 +1,9 @@
 // Scriptlet for uBlock Origin
 /// bandcampscriptlet.js
 (function() {
-    const css = `
-    .inline_player .prevbutton,
-    .inline_player .nextbutton {
-        background-image: url(/img/nextprevinvert.png) !important;
-    }`;
-
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.appendChild(document.createTextNode(css));
-    document.head.appendChild(style);
+    document.head.appendChild(
+        Object.assign(document.createElement("style"), {
+            textContent: ".inline_player .prevbutton,.inline_player .nextbutton {background-image: url(/img/nextprevinvert.png) !important;}"
+        })
+    );
 })();
